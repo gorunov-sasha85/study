c5d39ff9a5bfa6196f8b3b0ae796880b27423340
Alex Proger <gorunov.sasha85@gmail.com>
2018-06-02 18:16:14 +0300

    Прошел еще 2 таска

--- src/HTMLEditor/actions/RedoAction.java
+++ src/HTMLEditor/actions/RedoAction.java
@@ -12,6 +12,6 @@ public class RedoAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
+        view.redo();
     }
 }
--- src/HTMLEditor/actions/UndoAction.java
+++ src/HTMLEditor/actions/UndoAction.java
@@ -12,6 +12,6 @@ public class UndoAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
+        view.undo();
     }
 }
